7c839c021974b6127d986594de087030e5ad79a8
LTIUser <LTIUser@INFEDL31176.lntinfotech.com>
2021-09-23 17:48:11 -0400

    seachByName and lat implemented

---
+++ MyApplication/AppLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace AppLibrary
+{
+    public record GeoData(string name, double[] position);
+    public class Class1
+    {
+        
+        public static async Task<List<GeoData>> GetDataFromHttp(){
+
+            var client = new HttpClient();
+            var positions = await client.GetFromJsonAsync<List<GeoData>>("https://raw.githubusercontent.com/chyld/datasets/main/markers.json");
+
+            // if( positions != TaskStatus.RanToCompletion) throw new Exception("Json Error");
+
+            return positions;
+        }
+
+
+    }
+}
---
+++ MyApplication/MainApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using AppLibrary;
+
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var positions = Class1.GetDataFromHttp().GetAwaiter().GetResult();
+
+            foreach (var item in positions)
+            {
+            }
+            try
+            {
+                var x = searchByName(positions, "Disneyland");
+                var y = searchByLatitude(positions, 25.1212);
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+                    
+            }
+            catch (ArgumentException ex)
+            {
+                
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+        static GeoData searchByName(List<GeoData> geoData, string name1) => name1 switch
+                {
+                    "Grocery Store" => geoData[0],
+                    "Gas Station" => geoData[1],
+                    "Disneyland" => geoData[2],
+                    _ => throw new ArgumentException("Name not found"),
+                };
+        static GeoData searchByLatitude(List<GeoData> geoData, double lat) => lat switch
+                {
+                    25.1212 => geoData[0],
+                    12.3456 => geoData[1],
+                    4.8765 => geoData[2],
+                    _ => throw new ArgumentException("lat not found"),
+                };
+    }
+}
